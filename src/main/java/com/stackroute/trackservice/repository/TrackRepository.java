912de29c13ff879ea63ae33ee1739a50b622fca4
vijay1502 <52060554+vijay1502@users.noreply.github.com>
2019-08-11 23:56:21 +0530

    Update TrackRepository.java

--- src/main/java/com/stackroute/trackservice/repository/TrackRepository.java
+++ src/main/java/com/stackroute/trackservice/repository/TrackRepository.java
@@ -6,11 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+//@Repository annotation is used to provide the mechanism for storage and manipulation operation on objects
 @Repository
-public interface TrackRepository extends JpaRepository<Track, Integer> {
+//In this scenario we use h2 JPA Repository to store our data
 
+public interface TrackRepository extends JpaRepository<Track, Integer> {
+//Insert a query Annotation for using the database
     @Query("select t from Track t where t.trackName = ?1")
+    //Method to specify what type of OPeration we want to perform
     List<Track> getTrackByName(String trackName);
 
 }
