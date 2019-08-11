5836fe2974679dc083d9c562d97e4c9ffbfffd7b
vijay1502 <52060554+vijay1502@users.noreply.github.com>
2019-08-12 00:03:00 +0530

    Update Track.java

--- src/main/java/com/stackroute/trackservice/domain/Track.java
+++ src/main/java/com/stackroute/trackservice/domain/Track.java
@@ -4,9 +4,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+//An entity is a plain old Java object (POJO) class that is mapped to the database
+
 @Entity
+
+//Create a class for track in domain package
 public class Track {
+    
     @Id
+    //Create the fields in the track and make a contructor with args and without args
     private int trackId;
     private String trackName;
     private String comments;
@@ -16,7 +22,7 @@ public class Track {
         this.trackName = trackName;
         this.comments = comments;
     }
-
+    //Call the methods for given variables using getters and setters
     public int getTrackId() {
         return trackId;
     }
@@ -44,7 +50,7 @@ public class Track {
 
     public Track() {
     }
-
+    //Over ride the given parameters to represent them in a string format
     @Override
     public String toString() {
         return "Track{" +
@@ -54,3 +60,7 @@ public class Track {
                 '}';
     }
 }
+
+
+
+
