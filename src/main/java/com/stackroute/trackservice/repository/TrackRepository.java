68536103fdeb296a369ad77716f98baf2387f660
vijay1502 <52060554+vijay1502@users.noreply.github.com>
2019-08-12 00:19:30 +0530

    Update TrackRepository.java

--- src/main/java/com/stackroute/trackservice/repository/TrackRepository.java
+++ src/main/java/com/stackroute/trackservice/repository/TrackRepository.java
@@ -9,13 +9,11 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+//@Repository annotation is used to provide the mechanism for storage and manipulation operation on objects
 
 @Repository
+//In this scenario we use MOngoDb Repository to store our data
 public interface TrackRepository extends MongoRepository<Track, Integer> {
-
-    //    @Query("select t from Track t where t.trackName = ?1")
-//    List<Track> getTrackByName(String trackName);
-
-//    @Query
+//Insert a query Annotation for using the database for tracklist and find by its name
     List<Track> findByTrackName(String trackName);
 }
