578b51638091eb340d3f9033ce9d170c7abc2f80
vijay1502 <52060554+vijay1502@users.noreply.github.com>
2019-08-11 23:44:56 +0530

    Update TrackIdAlreadyExistsException.java

--- src/main/java/com/stackroute/trackservice/exceptions/TrackIdAlreadyExistsException.java
+++ src/main/java/com/stackroute/trackservice/exceptions/TrackIdAlreadyExistsException.java
@@ -1,6 +1,7 @@
 package com.stackroute.trackservice.exceptions;
-
+// Create an Exception for Track ID Already exists by extending root Exception
 public class TrackIdAlreadyExistsException extends Exception{
+    //Create a string message to send the output exception
     private String message;
     public TrackIdAlreadyExistsException() {
     }
