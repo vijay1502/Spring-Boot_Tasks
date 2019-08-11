bd75da457cc212dbfc45540fe4311b8e9fdc91dd
vijay1502 <52060554+vijay1502@users.noreply.github.com>
2019-08-12 00:01:24 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/trackservice/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/trackservice/service/TrackServiceImpl.java
@@ -9,17 +9,19 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+//@Service annotation use to write service components which should implement trackService methods
 @Service
 public class TrackServiceImpl implements TrackService {
+    //To be accessible only for classes in particular packages, we use private access modifier
     private TrackRepository trackRepository;
 
-
+//If connections are present, Wiring between different classes have to be done using @AutoWired annotation 
     @Autowired
     public TrackServiceImpl(TrackRepository trackRepository) {
         this.trackRepository = trackRepository;
     }
-
+   //Override the methods using @Override annotation
+  //This method will save the given track and throws exception if Id Already exists
     @Override
     public Track saveTrack(Track track) throws TrackIdAlreadyExistsException {
         if (trackRepository.existsById(track.getTrackId())) {
@@ -28,7 +30,9 @@ public class TrackServiceImpl implements TrackService {
         Track savedTrack = trackRepository.save(track);
         return savedTrack;
     }
-
+    
+    //This method gives track details, by Considering Id as INput
+    //If Id is not found, it should throw an exception 
     @Override
     public Optional<Track> getTrackById(int trackId) throws TrackNotFoundException {
 
@@ -43,6 +47,9 @@ public class TrackServiceImpl implements TrackService {
        List<Track> trackList=trackRepository.findAll();
        return trackList;
     }*/
+    
+     //This method gives all tracks details at once without any input
+    //If no tracks are available, it should throw an exception 
 
     @Override
     public List<Track> getAllTracks() throws Exception {
@@ -53,6 +60,9 @@ public class TrackServiceImpl implements TrackService {
         }
         return trackList;
     }
+    
+     //This method should delete track By Id and give remaining tracks
+    //If Id is not found, it should throw an exception
 
     @Override
     public Optional<Track> trackDeleteById(int trackId) throws TrackNotFoundException {
@@ -66,6 +76,9 @@ public class TrackServiceImpl implements TrackService {
         }
         return trackDelete;
     }
+    
+     //This method updates a given track, and show the output
+    //If Id is not found, it should throw an exception 
 
     @Override
     public Track updateTrack(int trackId, Track track) throws TrackNotFoundException {
@@ -80,6 +93,9 @@ public class TrackServiceImpl implements TrackService {
         return trackRepository.save(track);}
     }
 
+    
+     //using trackname,we get the track details
+    //If Id is not found, it should throw an exception 
     @Override
     public List<Track> getTrackByName(String trackName) throws TrackNotFoundException {
         List<Track> trackByName = trackRepository.getTrackByName(trackName);
@@ -89,3 +105,4 @@ public class TrackServiceImpl implements TrackService {
         return trackByName;
     }
 }
+
