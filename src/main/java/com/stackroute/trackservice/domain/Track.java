4503169502b1808fa6b224584b0bbd330807f5ed
vijay1502 <52060554+vijay1502@users.noreply.github.com>
2019-08-12 00:12:28 +0530

    Update Track.java

--- src/main/java/com/stackroute/trackservice/domain/Track.java
+++ src/main/java/com/stackroute/trackservice/domain/Track.java
@@ -1,28 +1,63 @@
 package com.stackroute.trackservice.domain;
 
-import jdk.jfr.DataAmount;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+//An entity is a plain old Java object (POJO) class that is mapped to the database
+
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
+//Create a class for track in domain package
 public class Track {
+    
     @Id
-
+    //Create the fields in the track and make a contructor with args and without args
     private int trackId;
-
     private String trackName;
-
     private String comments;
+
+    public Track(int trackId, String trackName, String comments) {
+        this.trackId = trackId;
+        this.trackName = trackName;
+        this.comments = comments;
+    }
+    //Call the methods for given variables using getters and setters
+    public int getTrackId() {
+        return trackId;
+    }
+
+    public void setTrackId(int trackId) {
+        this.trackId = trackId;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+
+    public Track() {
+    }
+    //Over ride the given parameters to represent them in a string format
+    @Override
+    public String toString() {
+        return "Track{" +
+                "trackId=" + trackId +
+                ", trackName='" + trackName + '\'' +
+                ", comments='" + comments + '\'' +
+                '}';
+    }
 }
+
