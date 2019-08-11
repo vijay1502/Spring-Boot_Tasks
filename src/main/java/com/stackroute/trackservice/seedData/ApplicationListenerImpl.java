9406c244b057390a17c9bedc0f872db280bf913d
vijay1502 <52060554+vijay1502@users.noreply.github.com>
2019-08-12 00:09:49 +0530

    Update ApplicationListenerImpl.java

--- src/main/java/com/stackroute/trackservice/seedData/ApplicationListenerImpl.java
+++ src/main/java/com/stackroute/trackservice/seedData/ApplicationListenerImpl.java
@@ -7,14 +7,17 @@ import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
-
 @Component
+//ApplicationListenerImpl should implement application listener to use seed data
 public class ApplicationListenerImpl implements ApplicationListener {
+    //Use Track Repository to send the data to its related database
     private TrackRepository trackRepository;
+    //Autowired with track repository
     @Autowired
         public ApplicationListenerImpl(TrackRepository trackRepository) {
         this.trackRepository = trackRepository;
     }
+    //Use on application event and create the hard coded data
     @Override
     public void onApplicationEvent(ApplicationEvent applicationEvent) {
         Track track5=new Track(1,"krishna","1st one");
