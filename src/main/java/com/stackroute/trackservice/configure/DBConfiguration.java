e38d1f6dd5f66970799f03e95601e91ebe2fa794
vijay1502 <52060554+vijay1502@users.noreply.github.com>
2019-08-12 07:55:13 +0530

    Update DBConfiguration.java

--- src/main/java/com/stackroute/trackservice/configure/DBConfiguration.java
+++ src/main/java/com/stackroute/trackservice/configure/DBConfiguration.java
@@ -11,9 +11,13 @@ public class DBConfiguration {
     private String userName;
     private String password;
     @ConfigurationProperties(prefix = "app")
+    //@Profile is logical grouping of data and can be used on type level annotation or asa meta data 
+    //dev represents the development,test represents testing and prod for production .
    @Profile("dev")
+    //Instantiating an object in spring using @Bean annotation
    @Bean
     public String  devDbConfiguraton(){
+       //Here we print data related to developer, the driver class name of database,url of it and return it to them
        System.out.println("DEV-DB-H2");
        System.out.println(driverClassName);
        System.out.println(url);
@@ -21,8 +25,11 @@ public class DBConfiguration {
 
    }
     @Profile("test")
+     //Instantiating an object in spring using @Bean annotation
     @Bean
     public String  testDbConfiguraton(){
+        //Here we print data related to testing, define the driver class name of database,url of it and return it to them
+
         System.out.println("Test using DataBase H2");
         System.out.println(driverClassName);
         System.out.println(url);
@@ -30,8 +37,11 @@ public class DBConfiguration {
 
     }
     @Profile("prod")
+     //Instantiating an object in spring using @Bean annotation
     @Bean
     public String  prodDbConfiguraton(){
+        //Here we print data related to production department, the driver class name of database,url of it and return it to them
+
         System.out.println("For Production through mySql");
 
         System.out.println(url);
